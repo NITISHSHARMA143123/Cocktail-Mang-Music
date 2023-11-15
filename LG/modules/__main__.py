1573886dc6a8c2ba461fa4c2e5ddec57f375a46a
Venom <141371272+NITISHSHARMA143123@users.noreply.github.com>
2023-11-16 04:46:03 +0530

    Update __main__.py

--- LG/modules/__main__.py
+++ LG/modules/__main__.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2022 @LOGI-LAB
+Copyright (c) 2022 @its_arryan
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
